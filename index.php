fede63a73873b1125e1331b039379292ac24066c
dpbam <derekphtpt@gmail.com>
2022-02-23 20:07:06 -0700

    arrays beginning

--- index.php
+++ index.php
@@ -1,3 +1,57 @@
 <?php
+// Single line comment
     echo 'Hello World';
+?>
+
+<?php
+/* multiple line 
+comment */
+?>
+
+<?php
+
+    // regular variable declaration
+    $age = 34;
+
+    // special variables called 'constants'
+
+    define('VERSION', 1.1);
+    echo VERSION;
+    // case sensitive
+    echo version; // will not work
+    // can't assign a new value to the variable
+    VERSION = 1.2; // will not work
+    define('VERSION', 1.2); // Also will not work
+
+    // double vs. single quotes
+    $name = "Joe";
+    echo "Hello, $name"; // will print Hello, Joe
+    echo 'Hello, $name'; // will print Hello, $name
+    echo 'Hello, ' . $name . '!'; // will print Hello, Joe!
+    echo 'Welcome to Joe\'s house!'; // will print Welcome to Joe's house! (with the escape character)
+
+    // 
+    echo 'This is line 1';
+    echo 'This is line 2'; //these will get printed on the same line
+    // 
+    echo "This is line 1 \n";
+    echo "This is line 2"; // these will print on different lines (they might render in the browser together, but check the source)
+
+    // Arrays
+
+    // indexable arrays - use numbers as keys, numbered lists that start at 0
+    // associative arrays - use strings as keys, Allows you to turn the numbered index into a string
+
+    Home Towns [
+        'Joe' => 'Middletown, NY'
+        'Erin' => 'Westchester, PA'
+        'Dave' => 'Exton, PA'
+        'Brian' => 'Grand Rapids, MI'
+    ]
+
+    // Multidemensional Arrays - an array of arrays
+
+
+
+
 ?>
